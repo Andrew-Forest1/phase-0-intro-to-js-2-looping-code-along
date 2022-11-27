74fec93a475dd95b065fe44fd91f9f01e7ed21ab
Andrew <aforest41@yahoo.com>
2022-11-27 12:37:05 -0800

    commit tree eef031e98d9292750375c583dc87cb0f14e2fcff

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(stringArray, eventName){
+    let newArray = [];
+    for(let index = 0; index < stringArray.length; index ++){
+        newArray.push(`Thank you, ${stringArray[index]}, for the wonderful ${eventName} gift!`)
+    }
+    return newArray;
+}
+
+function countDown(int){
+    while(int >= 0){
+        console.log(int);
+        int -= 1;
+    }
+}
